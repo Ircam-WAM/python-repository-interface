e026216f040209f9667fd489acca964e9000bb3d
Martin Desrumaux <martin@letik.io>
2022-01-27 16:55:24 +0100

    fix(get_readme): check with beautiful soup attr method

--- repository/vendors/utils.py
+++ repository/vendors/utils.py
@@ -73,7 +73,7 @@ class VendorMixin:
     def _process_img(self, html, default_branch="master"):
         soup = BeautifulSoup(html, features="html.parser")
         for img in soup.findAll('img'):
-            if 'src' not in img:
+            if not img.has_attr('src'):
                 continue
             # Github replaces blob URLs. e.g.:
             # - "https://github.com/COSIMA/logo/blob/master/png/logo_word.png"
